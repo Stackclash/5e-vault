a0fb558f193aa2fa0894aa36b68006757495e5c5
Richard Cox <cox.richard200@gmail.com>
2023-10-11 09:24:58 -0500

    vault backup: 2023-10-11 09:24:57

--- z_Scripts/Dataview/Region/locations.js
+++ z_Scripts/Dataview/Region/locations.js
@@ -1,3 +1,6 @@
-dv.execute(`TABLE WITHOUT ID link(file.name) AS "Name", file.frontmatter.pronounced as "Pronounced"
+console.log(dv.current())
+const poi = dv.pages('"4. World Almanac/Places of Interest"')
+const settlemtnts = dv.pages('"4. World Almanac/Settlements"')
+dv.execute(`TABLE WITHOUT ID link(file.name) AS "Name", file.folder as "Type", file.frontmatter.pronounced as "Pronounced"
             FROM "4. World Almanac/Places of Interest" OR "4. World Almanac/Settlements"
             WHERE contains(file.frontmatter.region_PoI, this.file.name) OR contains(file.frontmatter.region_settlement, this.file.name)`)
