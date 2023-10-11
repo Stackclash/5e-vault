12a6555f8ea938e1e712e94a82fa2aa4445bb736
Richard Cox <cox.richard200@gmail.com>
2023-10-11 09:30:03 -0500

    vault backup: 2023-10-11 09:30:02

--- z_Scripts/Dataview/Region/locations.js
+++ z_Scripts/Dataview/Region/locations.js
@@ -1,6 +1,11 @@
 console.log(dv.current())
 const poi = dv.pages('"4. World Almanac/Places of Interest"')
 const settlemtnts = dv.pages('"4. World Almanac/Settlements"')
-dv.execute(`TABLE WITHOUT ID link(file.name) AS "Name", file.folder as "Type", file.frontmatter.pronounced as "Pronounced"
-            FROM "4. World Almanac/Places of Interest" OR "4. World Almanac/Settlements"
-            WHERE contains(file.frontmatter.region_PoI, this.file.name) OR contains(file.frontmatter.region_settlement, this.file.name)`)
+const pages = poi.concat(settlemtnts).where(p => {
+    p.frontmatter.region_PoI.includes(dv.current().file.name) || p.frontmatter.region_settlement.includes(dv.current().file.name)
+})
+const tableValues = pages.map(p => [p.name, p.folder, p.frontmatter.pronounced])
+dv.table(["Name", "Type", "Pronounced"], tableValues)
+// dv.execute(`TABLE WITHOUT ID link(file.name) AS "Name", file.folder as "Type", file.frontmatter.pronounced as "Pronounced"
+//             FROM "4. World Almanac/Places of Interest" OR "4. World Almanac/Settlements"
+//             WHERE contains(file.frontmatter.region_PoI, this.file.name) OR contains(file.frontmatter.region_settlement, this.file.name)`)
