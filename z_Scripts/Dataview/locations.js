5fa8262a65d4c3c2b3fd29bfd9e43bff8b427934
Richard Cox <rick.cox@ssmhealth.com>
2024-01-18 18:53:58 -0600

    vault backup: 2024-01-18 18:53:58

--- z_Scripts/Dataview/locations.js
+++ z_Scripts/Dataview/locations.js
@@ -1,4 +1,7 @@
-const pages = dv.pages('"4. World Almanac"').groupBy(p => p.file.folder.split('/').slice(-1))//.where(p => p.location ? p.location == input.current.file.link : false).groupBy(p => p.folder.split('/').slice(-1))
+const pages = dv.pages('"4. World Almanac"').where(p => {
+    if (p.location) console.log(input.current.file, p.location)
+    return p.location && p.location.path ? p.location == input.current.file.link.path : false
+}).groupBy(p => p.file.folder.split('/').slice(-1))
 console.log(pages.length)
 for (let group of pages) {
     console.log(group)
