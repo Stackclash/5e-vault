24daab55fcafda433a90c307741ab6ff8a68cffa
Richard Cox <cox.richard200@gmail.com>
2024-01-19 13:10:17 -0600

    vault backup: 2024-01-19 13:10:17

--- z_Scripts/Templater/get_all_files.js
+++ z_Scripts/Templater/get_all_files.js
@@ -1,21 +1,26 @@
 const fs = require('fs')
 const path = require('path')
 
-function getAllFiles(folderPath, filesList=[]) {
-    const files = fs.readdirSync(folderPath)
+function getAllFiles(dv, folderPath, filesList=[]) {
+    // const files = fs.readdirSync(folderPath)
 
-    files.forEach(file => {
-        const filePath = path.resolve(folderPath, file)
-        const fileInfo = fs.statSync(filePath)
+    // files.forEach(file => {
+    //     const filePath = path.resolve(folderPath, file)
+    //     const fileInfo = fs.statSync(filePath)
 
-        if (fileInfo.isDirectory()) {
-            filesList.concat(goThroughFilesAndFolders(filePath, filesList))
-        } else {
-            filesList.push(path.basename(filePath))
-        }
-    });
+    //     if (fileInfo.isDirectory()) {
+    //         filesList.concat(goThroughFilesAndFolders(filePath, filesList))
+    //     } else {
+    //         filesList.push(path.basename(filePath))
+    //     }
+    // });
 
-    return filesList
+    // return filesList
+
+    let pages = dv.pages(folderPath).map(p => ({name: p.file.name, path: p.file.path}))
+    console.log(pages)
+
+    return pages
 }
 
 module.exports = getAllFiles
