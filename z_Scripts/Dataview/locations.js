3e65b31001befab2005e562a0f926b810d6cc892
Richard Cox <cox.richard200@gmail.com>
2024-01-19 10:57:23 -0600

    vault backup: 2024-01-19 10:57:23

--- z_Scripts/Dataview/locations.js
+++ z_Scripts/Dataview/locations.js
@@ -1,6 +1,8 @@
 const pages = dv.pages('"4. World Almanac"').where(p => {
-    if (p.location) console.log(input.current.file.link.path, p.location.path, p.location.path == input.current.file.link.path)
-    return p.location && p.location.path ? p.location.path == input.current.file.link.path : false
+    let currentPath = input.current.file.path
+
+    if (p.location) console.log(currentPath, p.location.path, p.location.path == currentPath)
+    return p.location && p.location.path ? p.location.path == currentPath : false
 }).groupBy(p => p.file.folder.split('/').slice(-1))
 for (let group of pages) {
     dv.header(3, group.key)
