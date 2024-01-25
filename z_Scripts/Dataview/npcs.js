f149264bf28671bfb9dcf7baa6a12c07cc7fe95a
Richard Cox <cox.richard200@gmail.com>
2024-01-25 13:03:39 -0600

    vault backup: 2024-01-25 13:03:38

--- z_Scripts/Dataview/npcs.js
+++ z_Scripts/Dataview/npcs.js
@@ -4,8 +4,20 @@ const pages = dv.pages('"4. World Almanac/NPCs"').where(p => {
     return p.location && p.location.path ? p.location.path == currentPath : false
 }).sort(p => p.file.name)
 
+pages.flatMap(p => {
+    let currentPage = p,
+    childLocations = [currentPage]
+
+    while (currentPage.location && currentPage.location.path) {
+        currentPage = dv.page(currentPage.location.path)
+        childLocations.concat(currentPage)
+    }
+
+    return childLocations
+})
+
 if (pages.length > 0) {
-    dv.table(["Name"],
-        pages.map(p => [p.file.link])
+    dv.table(["Name", "Location"],
+        pages.map(p => [p.file.link, p.location])
     )
 }
