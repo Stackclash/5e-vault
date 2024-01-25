b3176787fce468e21afe610b7a82eb4932191186
Richard Cox <cox.richard200@gmail.com>
2024-01-25 13:30:39 -0600

    vault backup: 2024-01-25 13:30:39

--- z_Scripts/Dataview/npcsInLocation.js
+++ z_Scripts/Dataview/npcsInLocation.js
@@ -1,16 +1,29 @@
 // This will only handle child locations one level down
-let currentPath = input.current.file.path
+const currentPath = input.current.file.path
 
-const childLocations = dv.pages('"4. World Almanac/Regions" or "4. World Almanac/Settlements" or "4. World Almanac/Places of Interest" or "4. World Almanac/Shops"').where(p => {
-    return p.location && p.location.path ? p.location.path == currentPath : false
-})
+function getChildLocationPaths(locationPath) {
+    return dv.pages('"4. World Almanac/Regions" or "4. World Almanac/Settlements" or "4. World Almanac/Places of Interest" or "4. World Almanac/Shops"').where(p => {
+        return p.location && p.location.path ? p.location.path == locationPath : false
+    }).map(p => p.file.path)
+}
 
-const childLocationPaths = childLocations.map(p => p.file.path)
+let childLocationPathsList = getChildLocationPaths(currentPath),
+currentListLength = childLocationPathsList.length
+
+do {
+    currentListLength = childLocationPathsList.length
+
+    childLocationPathsList = childLocationPathsList.flatMap(p => getChildLocationPaths(p))
+    //remove possible duplicates
+    childLocationPathsList = childLocationPathsList.filter((p, index) => childLocationPathsList.indexOf(p) == index)
+    
+    console.log("CALL --->>>", childLocationPathsList)
+
+} while (childLocationPathsList.length != currentListLength)
 
 const pages = dv.pages('"4. World Almanac/NPCs"').where(p => {
-    return p.location && p.location.path ? childLocationPaths.includes(p.location.path) : false
+    return p.location && p.location.path ? childLocationPathsList.includes(p.location.path) : false
 }).sort(p => dv.page(p.location.path).file.name)
-
 
 
 if (pages.length > 0) {
