2e9251a1dbefb62736091e2d993ccd333937a14c
Richard Cox <cox.richard200@gmail.com>
2024-01-25 15:00:39 -0600

    vault backup: 2024-01-25 15:00:38

--- z_Scripts/Dataview/npcRelationships.js
+++ z_Scripts/Dataview/npcRelationships.js
@@ -1,11 +1,13 @@
 let relationships = input.current
 const backticks = "```"
 
+relationships = relationships.map((r, index) => ({...r, key: String.fromCharCode(65 + index)}))
+
 console.log(relationships)
 
 dv.paragraph(
   `${backticks}mermaid
 graph LR
 ${relationships}
-${backticks} 
+${backticks}
 `)
