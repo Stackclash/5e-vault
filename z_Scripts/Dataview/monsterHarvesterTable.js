f86904c3448ef41c52c839cd5d2161bfc60eaa66
Richard Cox <rick.cox@ssmhealth.com>
2024-01-31 19:52:50 -0600

    vault backup: 2024-01-31 19:52:50

---
+++ z_Scripts/Dataview/monsterHarvesterTable.js
@@ -0,0 +1,6 @@
+let tablePage = dv.pages('"5. Mechanics/Tables"').filter(p => {
+    return p.file.name.includes("Hhhvi") &&
+        p.aliases[0] == input.current.aliases[0]
+})[0]
+
+dv.paragraph(dv.blockLink(tablePage.file.path, "dc-item-description-value-weight-crafting", true))
