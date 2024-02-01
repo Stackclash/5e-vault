2422b1503a98c474ed08700f0f3f52b16cb6918e
Richard Cox <rick.cox@ssmhealth.com>
2024-02-01 09:42:54 -0600

    vault backup: 2024-02-01 09:42:54

--- z_Scripts/Dataview/monsterHarvesterTable.js
+++ z_Scripts/Dataview/monsterHarvesterTable.js
@@ -6,7 +6,8 @@ let tablePage = dv.pages('"5. Mechanics/Tables"').filter(p => {
 if (tablePage) {
     dv.header(2, "Harvester Table")
     const table = await dv.io.load(dv.blockLink(tablePage.file.path, "dc-item-description-value-weight-crafting", true, input.current.aliases[0]))
-    console.log(table)
+    console.log(dv.fileLink(tablePage.file.path, true))
 
-    dv.el('span', table)
+    // dv.el('span', table)
+    dv.span(dv.fileLink(tablePage.file.path, true))
 }
