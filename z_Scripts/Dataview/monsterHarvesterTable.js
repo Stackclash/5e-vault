c5422f1a8ccb2e0da637a259761d233b2eaa1a1c
Richard Cox <cox.richard200@gmail.com>
2024-02-01 08:04:38 -0600

    vault backup: 2024-02-01 08:04:38

--- z_Scripts/Dataview/monsterHarvesterTable.js
+++ z_Scripts/Dataview/monsterHarvesterTable.js
@@ -6,5 +6,5 @@ let tablePage = dv.pages('"5. Mechanics/Tables"').filter(p => {
 if (tablePage) {
     dv.header(2, "Harvester Table")
     console.log(dv.blockLink(tablePage.file.path, "dc-item-description-value-weight-crafting", true, input.current.aliases[0]).toString().replace(' > ', '#^'))
-    dv.span(dv.blockLink(tablePage.file.path, "dc-item-description-value-weight-crafting", true, input.current.aliases[0]).toString().replace(' > ', '#^'))
+    dv.span(dv.blockLink(tablePage.file.path, "dc-item-description-value-weight-crafting", true, input.current.aliases[0]))
 }
