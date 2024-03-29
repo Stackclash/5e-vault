d62923ef77cb4088ba4ce088312fa1de32f8a782
Richard Cox <cox.richard200@gmail.com>
2024-03-29 11:11:48 -0500

    vault backup: 2024-03-29 11:11:48

--- index.js
+++ index.js
@@ -15,9 +15,61 @@ const journalMeta = {
     'prep-notes': ''
 }
 
+const journalContent = `> [!infobox]
+> ## Info
+> | | |
+> |---|---|
+> | **Party:** | \`INPUT[suggester(optionQuery("3. The Party/Parties")):party]\` |
+> | **Session Date:** | \`INPUT[datePicker:date]\` |
+> | **Prep Notes:** | \`INPUT[suggester(optionQuery("1. DM Stuff/Session Prep")):prep-notes]\` |
+> ## Fantasy Dates
+> | | |
+> |---|---|
+> | **Start Date:** | \`INPUT[text:fc-date]\`
+> | **End Date:** | \`INPUT[text:fc-end]\` |
+> | **Timelines:** | \`INPUT[inlineList:timelines]\` |
+# \`=this.file.name\`
+## Session Overview
+\`INPUT[textArea:summary]\`
+
+## Key Learnings
+Description of any important information that the party learned.
+Who Did They Meet?
+
+## Items Of Importance
+Description
+
+## What Happened
+Small description.`
+
 const prepMeta = {
     date: ''
 }
+
+const prepContent = `## Resources
+
+## Details to Remember
+
+## Strong Start  
+
+
+## Scenes  
+
+
+## Secrets and Clues  
+
+
+## Fantastic Locations  
+
+
+## Important NPCs  
+
+
+## Potential Monsters  
+
+
+## Potential Treasure  
+`
 
 const journalPath = './1. DM Stuff/Session Journals'
 const prepPath = './1. DM Stuff/Session Prep'
@@ -35,4 +87,6 @@ const preps = fs.readdirSync(prepPath) //.map(f => path.join(prepPath, f))
 
 for (const journal of journals) {
     console.log(journal)
+    process.exit()
+    // if (!preps.includes(journal.data.))
 }
