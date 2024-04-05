a761cd34f9a9c77e8dd1032a3c3a1ce1084bd601
Richard Cox <cox.richard200@gmail.com>
2024-04-05 09:32:31 -0500

    vault backup: 2024-04-05 09:32:30

--- z_Scripts/Dataview/test.js
+++ z_Scripts/Dataview/test.js
@@ -1,39 +1,54 @@
-const fs = require('fs')
-const path = require('path')
+// const fs = require('fs')
+// const path = require('path')
 
-const diceRollerPlugin = app.plugins.getPlugin('obsidian-dice-roller')
+// const diceRollerPlugin = app.plugins.getPlugin('obsidian-dice-roller')
 
-function goThroughFilesAndFolders(folderPath, filesList=[]) {
-    const files = fs.readdirSync(folderPath)
+// function goThroughFilesAndFolders(folderPath, filesList=[]) {
+//     const files = fs.readdirSync(folderPath)
 
-    files.forEach(file => {
-        const filePath = path.resolve(folderPath, file)
-        const fileInfo = fs.statSync(filePath)
+//     files.forEach(file => {
+//         const filePath = path.resolve(folderPath, file)
+//         const fileInfo = fs.statSync(filePath)
 
-        if (fileInfo.isDirectory()) {
-            filesList.concat(goThroughFilesAndFolders(filePath, filesList))
-        } else {
-            filesList.push(filePath)
-        }
-    });
+//         if (fileInfo.isDirectory()) {
+//             filesList.concat(goThroughFilesAndFolders(filePath, filesList))
+//         } else {
+//             filesList.push(filePath)
+//         }
+//     });
 
-    return filesList
-}
+//     return filesList
+// }
 
-async function getRandomMp3(list) {
-    return diceRollerPlugin.getArrayRoller(mp3s)
-    .then(arrayRoller => {
-        return arrayRoller.roll()
-    })
-    .then(({ results }) => {
-        return results
-    })
-}
+// async function getRandomMp3(list) {
+//     let results
 
-const mp3s = goThroughFilesAndFolders(app.vault.adapter.getBasePath()).filter(f => path.extname(f) == '.mp3')
+//     await diceRollerPlugin.getArrayRoller(mp3s)
+//     .then(arrayRoller => {
+//         return arrayRoller.roll()
+//     })
+//     .then((rollResults) => {
+//         console.log('HERE', rollResults.results)
+//         results = rollResults.results
+//     })
 
-const result = await getRandomMp3(mp3s)
+//     return results
+// }
 
-console.log(result)
+// const mp3s = goThroughFilesAndFolders(app.vault.adapter.getBasePath()).filter(f => path.extname(f) == '.mp3')
 
-dv.span(`![[${result}]]`)
+// const result = await getRandomMp3(mp3s)
+
+// console.log(result)
+
+// dv.span(`![[${result}]]`)
+
+const diceRollerPlugin = app.plugins.getPlugin("obsidian-dice-roller");
+const arrayRoller = await diceRollerPlugin.getArrayRoller([1, 2, 3, 4, 5], 2);
+
+// Roll the array roller
+await arrayRoller.roll();
+
+// Access the updated results
+const rolledResults = arrayRoller.results;
+dv.list([rolledResults]);
