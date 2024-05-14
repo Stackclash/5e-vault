debe03527b2bf44ceb73be3ce849bc4b32338598
Richard Cox <cox.richard200@gmail.com>
2024-05-14 16:06:36 -0500

    vault backup: 2024-05-14 16:06:36

--- z_Scripts/Dataview/dndBeyondFetch.js
+++ z_Scripts/Dataview/dndBeyondFetch.js
@@ -1,10 +1,12 @@
 const dndBeyondId = input.id
 const dndBeyondUrl = `https://character-service.dndbeyond.com/character/v5/character/${dndBeyondId}`
+console.log(dndBeyondUrl)
 
 const data = await fetch(dndBeyondUrl, {
-    headers: {
-        "Access-Control-Allow-Origin": "*"
-    }
+    // headers: {
+    //     "Access-Control-Allow-Origin": "*"
+    // }
+    mode: 'no-cors'
 })
 
 console.log(data)
