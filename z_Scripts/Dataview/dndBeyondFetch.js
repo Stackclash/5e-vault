e4bf5ddde0b44e9430cc01156e609d44b4e0396d
Richard Cox <cox.richard200@gmail.com>
2024-05-14 16:24:01 -0500

    vault backup: 2024-05-14 16:24:01

--- z_Scripts/Dataview/dndBeyondFetch.js
+++ z_Scripts/Dataview/dndBeyondFetch.js
@@ -1,9 +1,10 @@
 const fs = require('fs')
+const path = require('path')
 
 const dndBeyondId = input.id
 const dndBeyondUrl = `https://character-service.dndbeyond.com/character/v5/character/${dndBeyondId}`
 
 const data = JSON.parse(await request(dndBeyondUrl)).data
 
-fs.writeFileSync('test.json', JSON.stringify(data))
+fs.writeFileSync(path.join(app.vault.adapter.getBasePath(), 'test.json'), JSON.stringify(data))
 console.log('done')
