44039ec6d3aff37fc3fe31ca1b6442c580f17e1a
Richard Cox <rick.cox@ssmhealth.com>
2024-05-14 22:07:15 -0500

    vault backup: 2024-05-14 22:07:15

--- z_Scripts/Dataview/dndBeyondFetch.js
+++ z_Scripts/Dataview/dndBeyondFetch.js
@@ -6,3 +6,14 @@ const dndBeyondUrl = `https://character-service.dndbeyond.com/character/v5/chara
 
 const data = JSON.parse(await request(dndBeyondUrl)).data
 
+const ALIGNMENT = {
+    1: 'Lawful Good',
+    2: 'Neutral Good',
+    3: 'Chaotic Good',
+    4: 'Lawful Neutral',
+    5: 'True Neutral',
+    6: 'Chaotic Neutral',
+    7: 'Lawful Evil',
+    8: 'Neutral Evil',
+    9: 'Chaotic Evil'
+}
