f401ba23541094b1fd99b7bd20c0631214210273
Richard Cox <rick.cox@ssmhealth.com>
2024-05-15 10:03:38 -0500

    vault backup: 2024-05-15 10:03:38

--- z_Scripts/Dataview/dndBeyondFetch.js
+++ z_Scripts/Dataview/dndBeyondFetch.js
@@ -1,19 +1,44 @@
 const fs = require('fs')
 const path = require('path')
 
-const dndBeyondId = input.id
-const dndBeyondUrl = `https://character-service.dndbeyond.com/character/v5/character/${dndBeyondId}`
+const dndBeyondId = 105635812
 
-const data = JSON.parse(await request(dndBeyondUrl)).data
 
-const ALIGNMENT = {
-    1: 'Lawful Good',
-    2: 'Neutral Good',
-    3: 'Chaotic Good',
-    4: 'Lawful Neutral',
-    5: 'True Neutral',
-    6: 'Chaotic Neutral',
-    7: 'Lawful Evil',
-    8: 'Neutral Evil',
-    9: 'Chaotic Evil'
+class DnDBeyondCharacter {
+
+    static ABILITY_SCORES = {
+        1: 'Strength',
+        2: 'Dexerity',
+        3: 'Constitution',
+        4: 'Intelligence',
+        5: 'Wisdom',
+        6: 'Charisma'
+    }
+
+    static ALIGNMENT = {
+        1: 'Lawful Good',
+        2: 'Neutral Good',
+        3: 'Chaotic Good',
+        4: 'Lawful Neutral',
+        5: 'True Neutral',
+        6: 'Chaotic Neutral',
+        7: 'Lawful Evil',
+        8: 'Neutral Evil',
+        9: 'Chaotic Evil'
+    }
+
+    id;
+
+
+    constructor(id) {
+        this.id = id
+    }
+
+    async initialize() {
+        const data = JSON.parse(await request(`https://character-service.dndbeyond.com/character/v5/character/${dndBeyondId}`)).data
+
+
+    }
 }
+
+console.log(DnDBeyondCharacter)
