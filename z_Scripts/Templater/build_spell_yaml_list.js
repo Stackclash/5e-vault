2330e70ecce0129880e84998afbff197479e704f
Richard Cox <cox.richard200@gmail.com>
2024-05-24 21:48:30 -0500

    vault backup: 2024-05-24 21:48:30

--- z_Scripts/Templater/build_spell_yaml_list.js
+++ z_Scripts/Templater/build_spell_yaml_list.js
@@ -1,7 +1,20 @@
 module.exports = (tp, spells, path, spaces) => {
-    return tp.user.build_yaml_list(spells.map(item => ({
-        name: tp.user.find_file(item.name, path),
-        level: item.level,
-        isPrepared: item.isPrepared
-    })), spaces)
+    let result = []
+    if (typeof spells[0] === 'string') {
+        result = tp.user.build_yaml_list(spells.map(item => ({
+            name: tp.user.find_file(item.name, path),
+            level: item.level,
+            isPrepared: item.isPrepared
+        })), spaces)
+    } else {
+        result = spells.reduce((accum, spell) => {
+            return accum.push({
+                name: spell.name,
+                maxSpellSlots: tp.user.build_yaml_list(spell.spellSlots.max, spaces+2),
+                availableSpellSlots: tp.user.build_yaml_list(spell.spellSlots.max, spaces+2)
+            })
+        }, [])
+    }
+
+    return result
 }
--- z_Scripts/Templater/build_yaml_list.js
+++ z_Scripts/Templater/build_yaml_list.js
@@ -1,30 +1,42 @@
+const build_array_yaml_list = (list, spaces) => {
+  return list.reduce((accum, item) => {
+    const itemString = typeof item === 'string' ? `- "${item}"` : `- ${item}`
+    accum.push(itemString.padStart(spaces + itemString.length))
+    return accum
+  }, []).join(`\n`)
+}
+
+const build_object_yaml_list = (list, spaces) => {
+  return Object.entries(list).reduce((accum, [key, value]) => {
+    let finalSpaces = spaces
+    let itemString
+
+    if (typeof value === 'string') {
+      itemString = `${key}: "${value}"`
+    } else if (typeof value === 'number') {
+      itemString = `${key}: ${value}`
+    } else if (Array.isArray(value)) {
+      itemString = build_array_yaml_list(value, spaces+2)
+    } else if (typeof value === 'object') {
+      itemString = build_object_yaml_list(value, spaces+2)
+    }
+
+    if (accum.length === 0) {
+      itemString = `- ${itemString}`
+    } else {
+      finalSpaces += 2
+    }
+    accum.push(itemString.padStart(finalSpaces + itemString.length))
+
+    return accum
+  }, []).join(`\n`)
+}
+
 module.exports = (list, spaces) => {
   let result = []
   if (list.length > 0) {
-    if (typeof list[0] !== 'object') {
-      result = list.reduce((accum, item) => {
-        const itemString = typeof item === 'string' ? `- "${item}"` : `- ${item}`
-        accum.push(itemString.padStart(spaces + itemString.length))
-        return accum
-      }, []).join(`\n`)
-    } else {
-      result = list.reduce((accum, item) => {
-        accum.push(Object.entries(item).reduce((accum2, [key, value]) => {
-          let itemString = typeof value === 'string' ? `${key}: "${value}"` : `${key}: ${value}`
-          let finalSpaces = spaces
-          if (accum2.length === 0) {
-            itemString = `- ${itemString}`
-          } else {
-            finalSpaces += 2
-          }
-          accum2.push(itemString.padStart(finalSpaces + itemString.length))
-  
-          return accum2
-        }, []).join(`\n`))
-  
-        return accum
-      }, []).join(`\n`)
-    }
+    
+    result.join(`\n`)
     result = `\n${result}`
   } else {
     result = "[]"
