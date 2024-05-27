1644537eb7c572f53e069f06687914192e559daf
Richard Cox <rick.cox@ssmhealth.com>
2024-05-27 10:05:38 -0500

    vault backup: 2024-05-27 10:05:38

--- z_Scripts/Templater/build_yaml_list.js
+++ z_Scripts/Templater/build_yaml_list.js
@@ -8,7 +8,7 @@ const build_object_yaml_list = (object, spaces, config) => {
     if (typeof value === 'string') {
       let finalValue = value
       if (config && typeof config === 'object' && config.hasOwnProperty(key)) {
-        finalValue = tp.user.find_note(finalValue, config[key])
+        finalValue = tp.user.find_file(finalValue, config[key])
       }
 
       itemString = `${key}: "${finalValue}"`
@@ -36,11 +36,11 @@ const build_array_yaml_list = (list, spaces, config) => {
   if (list.length > 0) {
     list.forEach(item => {
       if (typeof item === 'string') {
-        itemString = `- "${config && typeof config === 'string' ? tp.user.find_note(item, config) : item}"`
+        itemString = `- "${config && typeof config === 'string' ? tp.user.find_file(item, config) : item}"`
       } else if (typeof item === 'number') {
         itemString = `- ${itemString}`
       } else if (typeof item === 'object') {
-        itemString = `- ${build_object_yaml_list(tp, item, spaces, config)}`
+        itemString = `- ${build_object_yaml_list(item, spaces, config)}`
       }
 
       result.push(itemString.padStart(spaces + itemString.length))
