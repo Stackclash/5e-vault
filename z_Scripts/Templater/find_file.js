6d698e768a99ee7868345b15b78da5b796e15941
Richard Cox <rick.cox@ssmhealth.com>
2024-06-02 09:10:24 -0500

    vault backup: 2024-06-02 09:10:24

--- z_Scripts/Templater/find_file.js
+++ z_Scripts/Templater/find_file.js
@@ -1,30 +1,37 @@
 const dv = app.plugins.getPlugin("dataview").api
-const minisearch = require('minisearch')
+const MiniSearch = require('minisearch')
 
-module.exports = (text, path) => {
-    const searchTerms = text.replace(/[\(\)']/gm,'').replace(/s$/, '').split(' ')
-    const scores = dv.pages(`"${path}"`).array().reduce((accum, page) => {
-        let score = 0
-        for (const term of searchTerms) {
-            if (page.file.name === text ||
-                (page.aliases && page.aliases[0] === text) ||
-                page.file.name === searchTerms.join(' ') ||
-                page.file.name === `${searchTerms.join(' ')} Armor`) {
-                score += 5
-            } else if (page.file.name.includes(term)) {
-                score++
-            }
-        }
-        accum.push({
-            value: page,
-            score
-        })
+module.exports = (text, searchPath) => {
+    // const searchTerms = text.replace(/[\(\)']/gm,'').replace(/s$/, '').split(' ')
+    // const scores = dv.pages(`"${path}"`).array().reduce((accum, page) => {
+    //     let score = 0
+    //     for (const term of searchTerms) {
+    //         if (page.file.name === text ||
+    //             (page.aliases && page.aliases[0] === text) ||
+    //             page.file.name === searchTerms.join(' ') ||
+    //             page.file.name === `${searchTerms.join(' ')} Armor`) {
+    //             score += 5
+    //         } else if (page.file.name.includes(term)) {
+    //             score++
+    //         }
+    //     }
+    //     accum.push({
+    //         value: page,
+    //         score
+    //     })
 
-        return accum
-    }, []).sort((a, b) => b.score - a.score)
-    // console.log(text, scores)
+    //     return accum
+    // }, []).sort((a, b) => b.score - a.score)
+    const pages = dv.pages(`"${searchPath}"`).array().map(p => ({name: p.file.name, path: p.file.path}))
+    const minisearch = new MiniSearch({
+        fields: ['name'],
+        storeFields: ['name', 'path']
+    })
+    minisearch.addAll(pages)
 
-    return scores[0].score === 0 || scores.filter(item => item.score === scores[0].score).length > 5 || scores[0].value.file.name.length > text.length * 2 ?
-        text :
-        `[[${scores[0].value.file.path}|${scores[0].value.file.name}]]`
+    const result = minisearch.search(text)
+    console.log(text, result)
+    // return scores[0].score === 0 || scores.filter(item => item.score === scores[0].score).length > 5 || scores[0].value.file.name.length > text.length * 2 ?
+    //     text :
+    //     `[[${scores[0].value.file.path}|${scores[0].value.file.name}]]`
 }
