c9e2777a7122e8e64f47e852044bb22fc4fa39b7
Richard Cox <rick.cox@ssmhealth.com>
2024-06-02 09:46:25 -0500

    vault backup: 2024-06-02 09:46:25

--- z_Scripts/Templater/find_file.js
+++ z_Scripts/Templater/find_file.js
@@ -3,31 +3,7 @@ const path = require('path')
 const MiniSearch = require(path.join(app.vault.adapter.getBasePath(), 'node_modules/minisearch/dist/cjs/index.cjs'))
 
 module.exports = (text, searchPath) => {
-    console.log(text, searchPath)
-    // const searchTerms = text.replace(/[\(\)']/gm,'').replace(/s$/, '').split(' ')
-    // const scores = dv.pages(`"${path}"`).array().reduce((accum, page) => {
-    //     let score = 0
-    //     for (const term of searchTerms) {
-    //         if (page.file.name === text ||
-    //             (page.aliases && page.aliases[0] === text) ||
-    //             page.file.name === searchTerms.join(' ') ||
-    //             page.file.name === `${searchTerms.join(' ')} Armor`) {
-    //             score += 5
-    //         } else if (page.file.name.includes(term)) {
-    //             score++
-    //         }
-    //     }
-    //     accum.push({
-    //         value: page,
-    //         score
-    //     })
-
-    //     return accum
-    // }, []).sort((a, b) => b.score - a.score)
-    const pages = dv.pages(`"${searchPath}"`).array().map((p,i) => {
-        console.log(p.aliases)
-        return {id: i,name: p.file.name, path: p.file.path, alias: p.aliases && p.aliases.length ? p.aliases[0] : ''}
-    })
+    const pages = dv.pages(`"${searchPath}"`).array().map((p,i) => ({id: i,name: p.file.name, path: p.file.path, alias: p.aliases && p.aliases.length ? p.aliases[0] : ''}))
     const minisearch = new MiniSearch({
         fields: ['name', 'alias'],
         storeFields: ['name', 'path']
@@ -35,9 +11,5 @@ module.exports = (text, searchPath) => {
     minisearch.addAll(pages)
 
     const results = minisearch.search(text)
-    console.log(text, results, results.length)
     return results.length === 0 ? text :`[[${results[0].path}|${results[0].name}]]`
-    // return scores[0].score === 0 || scores.filter(item => item.score === scores[0].score).length > 5 || scores[0].value.file.name.length > text.length * 2 ?
-    //     text :
-    //     `[[${scores[0].value.file.path}|${scores[0].value.file.name}]]`
 }
