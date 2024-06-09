b51df054acbc9a2e612b924eb8e67da7b27d3519
Richard Cox <rick.cox@ssmhealth.com>
2024-06-08 23:29:08 -0500

    vault backup: 2024-06-08 23:29:08

--- z_Scripts/Dataview/generateMetaBindInput.js
+++ z_Scripts/Dataview/generateMetaBindInput.js
@@ -1,8 +1,8 @@
 const locationConfig = dv.page('Configuration').locations
 const config = input
 
-const test = `${config.bindType}[${config.type}(optionQuery("1. DM Stuff/Session Prep")):${config.key}]`
+const test = `\`${config.bindType}[${config.type}(optionQuery("1. DM Stuff/Session Prep")):${config.key}]\``
 
 console.log(test)
 
-dv.span(test)
+dv.span('`BUTTON[test]`')
