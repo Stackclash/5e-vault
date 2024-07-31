8346810e8d19638e69618ac18268c7252bd4d740
Richard Cox <cox.richard200@gmail.com>
2024-07-31 10:34:01 -0500

    vault backup: 2024-07-31 10:34:00

--- weatherTest.js
+++ weatherTest.js
@@ -4,4 +4,9 @@ const path = require('path')
 
 const { seasons, months, climates, precipitations, winds } = matter(fs.readFileSync(path.join(__dirname, '1. DM Stuff/DM To-Do/Weather Generation.md'), 'utf8')).data
 
-console.log(seasons, months, climates, precipitations, winds)
+const getDayInYear = (date) => {
+    const [month, day] = date.split('-').map(Number)
+    return months.slice(0, month - 1).reduce((acc, curr) => acc + curr.length, 0) + day
+}
+
+console.log(getDayInYear('2-15')) // 365
