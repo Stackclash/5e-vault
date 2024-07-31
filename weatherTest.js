1a0d627ca3d9053551ef560fa2824dfdfa8946d7
Richard Cox <cox.richard200@gmail.com>
2024-07-31 11:39:02 -0500

    vault backup: 2024-07-31 11:39:02

--- weatherTest.js
+++ weatherTest.js
@@ -1,25 +1,35 @@
 const matter = require('gray-matter')
 const fs = require('fs')
 const path = require('path')
+const { get } = require('http')
 
 const { seasons, months, climates, precipitations, winds } = matter(fs.readFileSync(path.join(__dirname, '1. DM Stuff/DM To-Do/Weather Generation.md'), 'utf8')).data
 
 const getDayInYear = (date) => {
-    const [month, day] = date.split('-').map(Number)
+    const [month, day, year] = date.split('-').map(Number)
     return months.slice(0, month - 1).reduce((acc, curr) => acc + curr.length, 0) + day
 }
 
 const getTotalDaysInYear = () => months.reduce((acc, curr) => acc + curr.length, 0)
 
-const getDuration = (startDay, endDay) => endDay < startDay ? endDay + getTotalDaysInYear() - startDay + 1 : endDay - startDay + 1 
+const getDuration = (startDate, endDate) => {
+    const startDay = getDayInYear(startDate)
+    const endDay = getDayInYear(endDate)
+    return endDay < startDay ? endDay + getTotalDaysInYear() - startDay + 1 : endDay - startDay + 1
+}
 
-seasons.forEach(season => {
-    const { name, start, end } = season
-    const startDay = getDayInYear(start)
-    const endDay = getDayInYear(end)
-    console.log(`Season: ${name}`)
-    console.log(`Start: ${startDay}`)
-    console.log(`End: ${endDay}`)
-    console.log(`Duration: ${getDuration(startDay, endDay)} days`)
-    console.log()
-})
+const getSeason = (date) => {
+    let season = ''
+    const day = getDayInYear(date)
+    seasons.forEach(season => {
+        console.log(season.name, getDayInYear(season.start) <= day, day <= getDayInYear(season.end), day)
+        if ((getDayInYear(season.start) <= day || ) && day <= getDayInYear(season.end)) season = season.name
+    })
+    return season
+}
+
+const getTempRange = (climate, date) => {
+
+}
+
+console.log(getTotalDaysInYear()) // should return 'Winter'
