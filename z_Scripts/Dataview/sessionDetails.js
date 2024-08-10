d46fc92213480421e557ef53fbf6528546411bff
Richard Cox <rick.cox@ssmhealth.com>
2024-08-10 13:54:38 -0500

    vault backup: 2024-08-10 13:54:38

--- z_Scripts/Dataview/sessionDetails.js
+++ z_Scripts/Dataview/sessionDetails.js
@@ -1,5 +1,14 @@
-const details = dv.pages("#session-journal").file.lists.where(t => t.text.includes("#remember")).map((t) => {
-    return t.text.replace('#remember', '') + ' ' + dv.sectionLink(t.link.path, t.link.subpath, false, 'Go To')
-})
+const details = dv.pages("#session-journal").file.lists.where(t => t.text.includes("#remember")).array().reduce((acc, t) => {
+    console.log(t)
+    t.tags.forEach(tag => {
+        if (!tag.includes('remember') && !acc.some(a => a.key === tag)) {
+            acc.push({key: tag, rows: [t.text.replace('#remember', '') + ' ' + dv.sectionLink(t.link.path, t.link.subpath, false, 'Go To')]})
+        } else if (!tag.includes('remember')) {
+            acc.find(a => a.key === tag).rows.push(t.text.replace('#remember', '') + ' ' + dv.sectionLink(t.link.path, t.link.subpath, false, 'Go To'))
+        }
+    })
+    
+    return acc
+}, [])
 
 dv.list(dv.array(details))
