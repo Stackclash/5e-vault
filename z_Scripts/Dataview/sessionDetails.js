468d005d23d7063b6c356d2eaddfee9fdb30288e
Richard Cox <rick.cox@ssmhealth.com>
2024-08-10 14:04:41 -0500

    vault backup: 2024-08-10 14:04:41

--- z_Scripts/Dataview/sessionDetails.js
+++ z_Scripts/Dataview/sessionDetails.js
@@ -1,5 +1,4 @@
 const details = dv.pages("#session-journal").file.lists.where(t => t.text.includes("#remember")).array().reduce((acc, t) => {
-    console.log(t)
     t.tags.forEach(tag => {
         if (!tag.includes('remember') && !acc.some(a => a.key === tag)) {
             acc.push({key: tag, rows: [t.text.replace('#remember', '') + ' ' + dv.sectionLink(t.link.path, t.link.subpath, false, 'Go To')]})
@@ -21,3 +20,4 @@ details.forEach(d => {
     if (d.key) dv.header(2, d.key)
     dv.list(d.rows)
 })
+console.log('HERE')
