41223065595292062e8773cc6d69d213158d280d
Richard Cox <rick.cox@ssmhealth.com>
2024-08-12 10:07:24 -0500

    vault backup: 2024-08-12 10:07:24

--- z_Scripts/Dataview/sessionDetails.js
+++ z_Scripts/Dataview/sessionDetails.js
@@ -1,14 +1,21 @@
+const getText = (item) => item.text.replace('#remember', '') + ' ' + dv.sectionLink(item.link.path, item.link.subpath, false, 'Go To')
+
+const getKey = (key) => {
+    key = key.replace('#', '')
+    return key.charAt(0).toUpperCase() + key.slice(1)
+}
+
 const details = dv.pages("#session-journal").file.lists.where(t => t.text.includes("#remember")).array().reduce((acc, t) => {
     t.tags.forEach(tag => {
         if (!tag.includes('remember') && !acc.some(a => a.key === tag)) {
-            acc.push({key: tag, rows: [t.text.replace('#remember', '') + ' ' + dv.sectionLink(t.link.path, t.link.subpath, false, 'Go To')]})
+            acc.push({key: getKey, rows: [getText(t)]})
         } else if (!tag.includes('remember') && acc.some(a => a.key === tag)) {
-            acc.find(a => a.key === tag).rows.push(t.text.replace('#remember', '') + ' ' + dv.sectionLink(t.link.path, t.link.subpath, false, 'Go To'))
+            acc.find(a => a.key === tag).rows.push(getText(t))
         } else {
             if (acc.some(a => a.key === '')) {
-                acc.find(a => a.key === '').rows.push(t.text.replace('#remember', '') + ' ' + dv.sectionLink(t.link.path, t.link.subpath, false, 'Go To'))
+                acc.find(a => a.key === '').rows.push(getText(t))
             } else {
-                acc.push({key: '', rows: [t.text.replace('#remember', '') + ' ' + dv.sectionLink(t.link.path, t.link.subpath, false, 'Go To')]})
+                acc.push({key: '', rows: [getText(t)]})
             }
         }
     })
@@ -20,4 +27,3 @@ details.forEach(d => {
     if (d.key) dv.header(2, d.key)
     dv.list(d.rows)
 })
-console.log('HERE')
