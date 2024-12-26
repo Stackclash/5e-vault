57991a21e1dd5a3427485760d4a513c54279bd38
Richard Cox <rick.cox@ssmhealth.com>
2024-12-26 11:28:09 -0600

    vault backup: 2024-12-26 11:28:09

--- z_Scripts/JS/downloadExternalFiles.js
+++ z_Scripts/JS/downloadExternalFiles.js
@@ -9,17 +9,14 @@ if (!config) {
     {
       description: 'ttrpg-convert-cli Styles',
       destination: '.obsidian/snippets/ttrpg-convert-cli.css',
-      urls: [
-        'https://rawcdn.githack.com/ebullient/ttrpg-convert-cli/a19cbe604f67f472c9956d61410fcf5defe5c93a/examples/css-snippets/compendium-obsidian-0.15.css',
-        'https://rawcdn.githack.com/ebullient/ttrpg-convert-cli/a19cbe604f67f472c9956d61410fcf5defe5c93a/examples/css-snippets/dnd5e-compendium.css',
-      ],
+      urls: 'https://rawcdn.githack.com/ebullient/ttrpg-convert-cli/refs/heads/main/examples/css-snippets/dnd5e-compendium.css'
     },
     {
       description: 'ITS Theme CSS Styles',
       destination: '.obsidian/snippets/ITS-Theme.css',
       urls: [
-        'https://rawcdn.githack.com/SlRvb/Obsidian--ITS-Theme/abb78a18f2bf71fc5b5e06f63f5724f53c534bc3/Snippets/S%20-%20Encoded%20Fonts.css',
-        'https://rawcdn.githack.com/SlRvb/Obsidian--ITS-Theme/abb78a18f2bf71fc5b5e06f63f5724f53c534bc3/Snippets/S%20-%20Encoded%20Icons.css'
+        'https://rawcdn.githack.com/SlRvb/Obsidian--ITS-Theme/refs/heads/main/Snippets/S%20-%20Encoded%20Fonts.css',
+        'https://rawcdn.githack.com/SlRvb/Obsidian--ITS-Theme/refs/heads/main/Snippets/S%20-%20Encoded%20Icons.css'
       ]
     },
     {
@@ -41,7 +38,12 @@ config.forEach(config => {
 })
 
 Promise.all(promises)
-.then(data => {
-  const css = data.filter(data => data).join('\n')
-  fs.writeFileSync(path.join(app.vault.adapter.getBasePath(), '.obsidian/snippets/External CSS.css'), css)
+.then(responses => {
+  responses.forEach((data, index) => {
+    if (Array.isArray(data)) {
+      fs.writeFileSync(path.join(app.vault.adapter.getBasePath(), config[index].destination), data.join('\n\n'))
+    } else {
+      fs.writeFileSync(path.join(app.vault.adapter.getBasePath(), config[index].destination), data)
+    }
+  })
 })
