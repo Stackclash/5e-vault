cb3b59885f14b3bb3eed384676b58a7511c7b1c1
Richard Cox <rick.cox@ssmhealth.com>
2024-12-26 11:11:53 -0600

    vault backup: 2024-12-26 11:11:52

--- z_Scripts/JS/downloadExternalFiles.js
+++ z_Scripts/JS/downloadExternalFiles.js
@@ -4,21 +4,26 @@ if (!path) {
 if (!fs) {
   const fs = require('fs')
 }
-if (!cssUrls) {
-  const cssUrls = [
+if (!config) {
+  const config = [
     {
       description: 'ttrpg-convert-cli Styles',
+      destination: '.obsidian/snippets/ttrpg-convert-cli.css',
       urls: [
         'https://rawcdn.githack.com/ebullient/ttrpg-convert-cli/a19cbe604f67f472c9956d61410fcf5defe5c93a/examples/css-snippets/compendium-obsidian-0.15.css',
         'https://rawcdn.githack.com/ebullient/ttrpg-convert-cli/a19cbe604f67f472c9956d61410fcf5defe5c93a/examples/css-snippets/dnd5e-compendium.css',
-      ]
+      ],
     },
     {
       description: 'ITS Theme CSS Styles',
+      destination: '.obsidian/snippets/ITS-Theme.css',
       urls: [
         'https://rawcdn.githack.com/SlRvb/Obsidian--ITS-Theme/abb78a18f2bf71fc5b5e06f63f5724f53c534bc3/Snippets/S%20-%20Encoded%20Fonts.css',
         'https://rawcdn.githack.com/SlRvb/Obsidian--ITS-Theme/abb78a18f2bf71fc5b5e06f63f5724f53c534bc3/Snippets/S%20-%20Encoded%20Icons.css'
       ]
+    },
+    {
+
     }
   ]
 }
@@ -27,13 +32,16 @@ if (!promises) {
 }
 promises = []
 
-cssUrls.forEach(url => {
-  promises.push(request(url))
+config.forEach(config => {
+  if (Array.isArray(config.urls)) {
+    promises.push(Promise.all(config.urls.map(url => request(url))))
+  } else {
+    promises.push(request(url))
+  }
 })
 
 Promise.all(promises)
 .then(data => {
-  console.log(data)
   const css = data.filter(data => data).join('\n')
   fs.writeFileSync(path.join(app.vault.adapter.getBasePath(), '.obsidian/snippets/External CSS.css'), css)
 })
