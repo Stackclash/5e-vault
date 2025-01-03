07523b147f1f2bb3c70fa5748c48b38fad0b8e93
Richard Cox <cox.richard200@gmail.com>
2025-01-03 13:48:56 -0600

    vault backup: 2025-01-03 13:48:56

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 try {
   const path = require('path')
   const fs = require('fs')
-  const url = require('url')
   const https = require('https')
   
   async function request(url) {
@@ -22,44 +21,29 @@ try {
 
   const config = [
       {
-        description: 'ttrpg-convert-cli Styles',
-        destination: '.obsidian/snippets/ttrpg-convert-cli.css',
-        urls: 'https://rawcdn.githack.com/ebullient/ttrpg-convert-cli/refs/heads/main/examples/css-snippets/dnd5e-compendium.css'
-      },
-      {
-        description: 'ITS Theme CSS Styles',
-        destination: '.obsidian/snippets/ITS-Theme.css',
+        description: 'Custom CSS',
+        destination: '.obsidian/snippets',
         urls: [
+          'https://rawcdn.githack.com/ebullient/ttrpg-convert-cli/refs/heads/main/examples/css-snippets/dnd5e-compendium.css',
           'https://rawcdn.githack.com/SlRvb/Obsidian--ITS-Theme/refs/heads/main/Snippets/S%20-%20Encoded%20Fonts.css',
-          'https://rawcdn.githack.com/SlRvb/Obsidian--ITS-Theme/refs/heads/main/Snippets/S%20-%20Encoded%20Icons.css'
+          'https://rawcdn.githack.com/SlRvb/Obsidian--ITS-Theme/refs/heads/main/Snippets/S%20-%20Encoded%20Icons.css',
+          'https://rawcdn.githack.com/sailKiteV/Obsidian-Snippets-and-Demos/refs/heads/main/TabsInObsidian/TabsInObsidian.css'
         ]
-      },
-      {
-        description: 'Metabind Tabs',
-        destination: '.obsidian/snippets/metabind-tabs.css',
-        urls: 'https://rawcdn.githack.com/sailKiteV/Obsidian-Snippets-and-Demos/refs/heads/main/TabsInObsidian/TabsInObsidian.css'
       }
     ]
   
   let promises = []
-  console.log(decodeURI(path.basename(url.parse(config[1].urls[0]).pathname)))
   
   config.forEach(item => {
-    if (Array.isArray(item.urls)) {
-      promises.push(Promise.all(item.urls.map(url => request(url))))
-    } else {
-      promises.push(request(item.urls))
-    }
+    promises.push(Promise.all(item.urls.map(url => request(url))))
   })
   
   Promise.all(promises)
   .then(responses => {
     return responses.forEach((data, index) => {
-      if (Array.isArray(data)) {
-        fs.writeFileSync(path.join(/* app.vault.adapter.getBasePath() */__dirname, config[index].destination), data.join('\n\n'))
-      } else {
-        fs.writeFileSync(path.join(/* app.vault.adapter.getBasePath() */__dirname, config[index].destination), data)
-      }
+      data.forEach((item, i) => {
+        fs.writeFileSync(path.join(/* app.vault.adapter.getBasePath() */__dirname, config[index].destination, decodeURI(path.basename(new URL(config[index].urls[i]).pathname))), item)
+      })
     })
   })
   .then(() => {
