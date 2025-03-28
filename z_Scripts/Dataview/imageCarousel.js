6d17315055a028ec9a5a06bd8d7bf6d97d619b61
Richard Cox <rick.cox@ssmhealth.com>
2025-03-28 13:04:50 -0500

    vault backup: 2025-03-28 13:04:50

--- z_Scripts/Dataview/imageCarousel.js
+++ z_Scripts/Dataview/imageCarousel.js
@@ -3,14 +3,13 @@ const backticks = "```"
 let carouselImageString = ''
 
 if (Array.isArray(images)) {
-  console.log('ARRAY')
   carouselImageString = images.join(', ')
 } else {
-  console.log('NOT ARRAY')
   carouselImageString = images
 }
-console.log(carouselImageString)
 
-dv.paragraph(`${backticks}carousel
-images: ${carouselImageString}
-${backticks}`)
+if (carouselImageString.length > 0) {
+  dv.paragraph(`${backticks}carousel
+  images: ${carouselImageString}
+  ${backticks}`)
+}
