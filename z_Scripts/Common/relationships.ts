a2d1e2a6eb3f584d54f563f825efee2296b3b9ee
Richard Cox <rick.cox@ssmhealth.com>
2025-03-29 01:44:01 -0500

    vault backup: 2025-03-29 01:44:01

--- z_Scripts/Common/relationships.ts
+++ z_Scripts/Common/relationships.ts
@@ -1,8 +1,13 @@
 // { "language": "TypeScript" }
 const dv = app.plugins.getPlugin("dataview").api
 
-export const getRelationships = (search: string, frontMatter: string) => {
+export const getRelationships = (search: string, frontMatter: string, options) => {
     const activeFile = dv.page(app.workspace.getActiveFile().path)
+    const searchPages = dv.pages(search)
 
-    console.log(activeFile)
+    if (typeof activeFile[frontMatter] === 'string') {
+        return searchPages.where(p => {
+            return activeFile[frontMatter]
+        })
+    }
 }
