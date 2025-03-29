965b0b4bf21b9e0e79410a6a3e1ec0b3573183b6
Richard Cox <rick.cox@ssmhealth.com>
2025-03-29 00:32:50 -0500

    vault backup: 2025-03-29 00:32:50

--- z_Scripts/Common/getConfig.ts
+++ z_Scripts/Common/getConfig.ts
@@ -1,9 +1,14 @@
 // { "language": "TypeScript" }
 const path = await self.require.import('path')
+const fs = await self.require.import('fs')
 const matter = await self.require.import('https://esm.sh/gray-matter')
+const config = await self.require.import('../../1. DM Stuff/Configuration.md')
 
 const main = () => {
-    const data = matter.read(path.resolve(__dirname, '../../1. DM Stuff/Configuration.md')).data
+    const content = fs.readFileSync('../../1. DM Stuff/Configuration.md', 'utf-8')
+    console.log(content)
+    const data = matter(content).data
+    console.log(data)
     return data
 }
 
