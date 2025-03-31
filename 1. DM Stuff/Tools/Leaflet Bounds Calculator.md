238c1a20e63a7b121752902d3247d85149ca9050
Richard Cox <cox.richard200@gmail.com>
2025-03-31 13:17:09 -0500

    vault backup: 2025-03-31 13:17:08

--- 1. DM Stuff/Tools/Leaflet Bounds Calculator.md
+++ 1. DM Stuff/Tools/Leaflet Bounds Calculator.md
@@ -3,15 +3,21 @@ image: z_Assets/Locations/Maps/Castle Ravenloft Courtyard.webp
 defaultZoom: -1
 minZoom: -2.5
 maxZoom: 1.5
+unit: ft
 ---
 
 > [!infobox|right]
 > ```meta-bind
 > INPUT[imageSuggester(optionQuery("z_Assets")):image]
 > ```
-> **Default Zoom:** `INPUT[number:defaultZoom]`
-> **Min Zoom:** `INPUT[number:minZoom]`
-> **Max Zoom:** `INPUT[number:maxZoom]`
+> **Default Zoom:**
+> `INPUT[number:defaultZoom]`
+> **Min Zoom:**
+> `INPUT[number:minZoom]`
+> **Max Zoom:**
+> `INPUT[number:maxZoom]`
+> **Unit:**
+> `INPUT[text:unit]`
 
 ```dataviewjs
 await dv.view('leafletConfig', {current:dv.current()})
--- z_Scripts/Dataview/leafletConfig.js
+++ z_Scripts/Dataview/leafletConfig.js
@@ -9,8 +9,7 @@ const backticks = "```"
 const readStream = createReadStream(path.join(app.vault.adapter.getBasePath(), currentPage.image))
 const { height: imageHeight, width: imageWidth } = await imageDimensionsFromStream(readStream)
 
-const mapMarkers = leafletConfig.mapMarkers.find(m => m.id === 'MapCalcExample')
-
+const mapMarker = leafletConfig.mapMarkers.find(m => m.id === 'MapCalcExample')[0]
 
 const leafletMapConfig = `id: MapCalcExample ### Must be unique with no spaces  
 image: [[${currentPage.image}]] ### Link to the map image file. Do not add a ! in front of the image  
@@ -23,7 +22,7 @@ minZoom: ${currentPage.minZoom} ### Controls how far away from the map you can z
 maxZoom: ${currentPage.maxZoom} ### Controls how far towards the map you can zoom in. Hover over the target icon to see the current level.  
 defaultZoom: ${currentPage.defaultZoom} ### Sets the default zoom level when the map loads. Hover over the target icon to see the current level.  
 zoomDelta: 0.5 ### Adjust how much the zoom changes when you zoom in or out.  
-unit: mi ### The value displayed when measuring so you know what type of unit is being measure.  
+unit: ${currentPage.unit} ### The value displayed when measuring so you know what type of unit is being measure.  
 scale: 0.09328358208955223 ### Real units/px (resolution) of your map  
 recenter: false  
 darkmode: false ### marker`
