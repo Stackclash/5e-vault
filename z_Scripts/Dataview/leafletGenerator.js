b3db77e95befce473fabdc6c7551469a020b520a
Richard Cox <cox.richard200@gmail.com>
2025-03-31 12:46:12 -0500

    vault backup: 2025-03-31 12:46:12

--- z_Scripts/Dataview/leafletGenerator.js
+++ z_Scripts/Dataview/leafletGenerator.js
@@ -7,17 +7,14 @@ const path = await self.require.import('path')
 const readStream = createReadStream(path.join(app.vault.adapter.getBasePath(), currentPage.image))
 const { height: imageHeight, width: imageWidth } = await imageDimensionsFromStream(readStream)
 
-
-console.log(currentPage.image)
-
 dv.paragraph(`${backticks}leaflet  
 id: MapCalcExample1 ### Must be unique with no spaces  
 image: [[${currentPage.image}]] ### Link to the map image file. Do not add a ! in front of the image  
 bounds: [[0,0], [2048, 1642]] ### Size of the map in px Height_y, Width_x. Ignore 0,0  
-height: ${imageHeight}px ### Size of the leaflet embed in px on your screen  
+height: 750px ### Size of the leaflet embed in px on your screen  
 width: 95% ### Size of the leaflet embed in your note  
-lat: 1024 ### To center the map, make this half of the map height.  
-long: 821 ### To center the map, make this half of the map width.  
+lat: ${imageHeight/2} ### To center the map, make this half of the map height.  
+long: ${imageWidth/2} ### To center the map, make this half of the map width.  
 minZoom: -1.5 ### Controls how far away from the map you can zoom out. Hover over the target icon to see the current level.  
 maxZoom: 1 ### Controls how far towards the map you can zoom in. Hover over the target icon to see the current level.  
 defaultZoom: -1 ### Sets the default zoom level when the map loads. Hover over the target icon to see the current level.  
