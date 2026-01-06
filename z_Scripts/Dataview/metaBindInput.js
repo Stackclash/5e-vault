a61947f0b610cd94c53c0a0259551614c50fbaa2
Richard Cox <cox.richard200@gmail.com>
2026-01-06 16:43:30 -0600

    vault backup: 2026-01-06 16:43:30

--- z_Scripts/Dataview/metaBindInput.js
+++ z_Scripts/Dataview/metaBindInput.js
@@ -3,4 +3,4 @@ const path = input.path || ''
 const field = input.field
 const options = input.options || []
 
-dv.span(`\`INPUT[${inputType}${options.length > 0 ? `(${options.join(',')})` : ''}:${path ? path + '#' : ''}${Array.isArray(field) ? field.map(f => `["${f}"]`).join('') : `["${field}"]`}]\``)
+dv.span(`\`INPUT[${inputType}${options.length > 0 ? `(${options.join(',')})` : ''}:${path ? path + '#' : ''}${Array.isArray(field) ? field.map(f => typeof f === 'string' ? `["${f}"]` : `[${f}]`).join('') : (typeof field === 'string' ? `["${field}"]` : `[${field}]`)}]\``)
