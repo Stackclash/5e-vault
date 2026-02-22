aa2153babc43d9b0b4a89f86420accafb606657d
Richard Cox <cox.richard200@gmail.com>
2026-02-22 17:59:04 -0600

    vault backup: 2026-02-22 17:59:04

--- z_Scripts/Dataview/utils/generateMetaBindInput.js
+++ z_Scripts/Dataview/utils/generateMetaBindInput.js
@@ -2,5 +2,7 @@ export function generateMetaBindInput(metaBind) {
     if (!metaBind) return ''
     const { type = 'text', path = '', field, options = [] } = metaBind
 
-    return `\`INPUT[${type}${options.length > 0 ? `(${options.join(',')})` : ''}:${path ? path + '#' : ''}${Array.isArray(field) ? field.map(f => typeof f === 'string' ? `["${f}"]` : `[${f}]`).join('') : (typeof field === 'string' ? `["${field}"]` : `[${field}]`)}]\``
+    const fieldArray = Array.isArray(field) ? field : [field]
+
+    return `\`INPUT[${type}${options.length > 0 ? `(${options.join(',')})` : ''}:${path ? path + '#' : ''}${fieldArray.map(f => typeof f === 'string' ? `["${f}"]` : `[${f}]`).join('')}]\``
 }
